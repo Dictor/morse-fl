e1ec402a75164a553f3a046a576c50535b82d53f
Dictor <kimdictor@gmail.com>
2022-10-03 17:51:16 +0900

    Add hardware initialization logic

--- src/app_main.cpp
+++ src/app_main.cpp
@@ -1,13 +1,37 @@
+#include "../inc/app_main.h"
+
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/zephyr.h>
 
+#include "../inc/hardware.h"
 #include "../inc/version.h"
-#include "../inc/app_main.h"
 
 LOG_MODULE_REGISTER(app_main);
 
+using namespace hangang_view;
+
 void AppMain(void) {
+  /* hardware initialization */
+  LOG_INF("hardware initialization start");
+  if (int ret = hardware::CheckHardware() < 0) {
+    LOG_ERR("fail to check hardware, ret=%d", ret);
+    return;
+  }
+  if (int ret = hardware::InitHardware() < 0) {
+    LOG_ERR("fail to initiate hardware, ret=%d", ret);
+    return;
+  }
+  LOG_INF("hardware initialization complete");
+
+  /* application logic */
   LOG_INF("application started");
   LOG_INF("hangang-view %d.%d.%d", hangang_view::kMajorVersion,
           hangang_view::kMinorVersion, hangang_view::kHotfixVersion);
+
+  for (;;) {
+    gpio_pin_toggle_dt(&hangang_view::hardware::run_led);
+    k_sleep(K_MSEC(1000));
+  }
 }
